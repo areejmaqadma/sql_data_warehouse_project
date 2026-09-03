89285e361344714bbc46ea7f328c9d8fe9a0dbee
areejmaqadma <136124541+areejmaqadma@users.noreply.github.com>
2026-09-03 12:13:22 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,37 @@
+/* 
+==============================================================
+Create Database and Schemas
+==============================================================
+Script Purpose:
+
+
+
+
+
+
+
+
+
+*/
+
+--Drop and recreate the "datawarehouse" database
+if EXISTS (SELECT 1 FROM sys.databases WHERE name="datawarehouse")
+BEGIN
+  ALTER database datawarehose SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE datawarehouse;
+
+END;
+GO
+
+
+
+--Create Database "datawarehouse"
+CREATE database datawarehouse;
+use datawarehouse;
+
+CREATE schema bronze;
+
+CREATE schema silver;
+
+CREATE schema gold;
 
